216c4de2fdc285efbba1e54a4c6855323056e62e
ruslannnnnnnnn <unknownuser51472@gmail.com>
2025-01-21 23:47:04 +0500

    fix Task struct

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module repos/task_manager
 go 1.23.5
 
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-sqlite3 v1.14.24
 )
--- src/model/task_model.go
+++ src/model/task_model.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Task struct {
-	Id          int       `json:id`
-	Title       string    `json:title`
-	Description string    `json:description`
-	CreatedAt   time.Time `json:created_at`
+	Id          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
 }
 
 func Get() string {
